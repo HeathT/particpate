fb229b3278a420f9b6e249e5227dcacc8d3d9a8e
Heath Thompson <heath.thompson@gmail.com>
2018-01-11 15:41:44 -0600

    Dropping that sweet init
    holy sheeut mofacker this better work

--- init.js
+++ init.js
@@ -7,5 +7,84 @@
 
 
 
+// create a global variable for our application - this will later need to be minified and obfiscated
+var _ = _ || {};
+
+
+// specify which libraries to load as part of this init
+Ext.require([
+	'Ext.tab.*',
+	'Ext.window.*',
+	'Ext.tip.*',
+	'Ext.layout.container.Border'
+]);
+
+
+
+// once Ext is ready do all the things - ALL THE THINGS
+Ext.onReady(function(){
+	var win,
+		button = Ext.get('show-btn');
+	
+
+	if (!win) {
+		win = Ext.create('widget.window', {
+			title: 'Layout Window with title <em>after</em> tools',
+			header: {
+				titlePosition: 2,
+				titleAlign: 'center'
+			},
+			closable: true,
+			closeAction: 'hide',
+			maximizable: true,
+			animateTarget: button,
+			width: 600,
+			minWidth: 350,
+			height: 350,
+			tools: [{type: 'pin'}],
+			layout: {
+				type: 'border',
+				padding: 5
+			},
+			items: [{
+				region: 'west',
+				title: 'Navigation',
+				width: 200,
+				split: true,
+				collapsible: true,
+				floatable: false
+			}, {
+				region: 'center',
+				xtype: 'tabpanel',
+				items: [{
+					// LTR even when example is RTL so that the code can be read
+					rtl: false,
+					title: 'Bogus Tab',
+					html: '<p>Window configured with:</p><pre style="margin-left:20px"><code>header: {\n    titlePosition: 2,\n    titleAlign: "center"\n},\nmaximizable: true,\ntools: [{type: "pin"}],\nclosable: true</code></pre>'
+				}, {
+					title: 'Another Tab',
+					html: 'Hello world 2'
+				}, {
+					title: 'Closable Tab',
+					html: 'Hello world 3',
+					closable: true
+				}]
+			}]
+		});
+	}
+	button.dom.disabled = true;
+	if (win.isVisible()) {
+		win.hide(this, function() {
+			button.dom.disabled = false;
+		});
+	} else {
+		win.show(this, function() {
+			button.dom.disabled = false;
+		});
+	}
+});
+
+
+
 
 
